96caf1bd3fedaebcd5cd462f5c508e63e37cdb3b
JacobGriffithElliott <j.o.gelliott@gmail.com>
2021-07-06 13:28:46 -0700

    1-5 Complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+// Listen event 1 - Mouseover
+let nameMouse = document.querySelector('.nav')
+
+nameMouse.addEventListener('mouseover', function(event){
+    event.target.style.color = "orange";
+    
+    setTimeout(function(){
+        event.target.style.color = "";
+    }, 500);
+})
+
+// Listen event 2 - Copy event
+document.addEventListener('copy', (event) => {
+    console.log('Items copied')
+});
+
+// Listen event 3 - Pointer Over
+document.addEventListener('pointerover', (event) => {
+    console.log('Pointer moved in');
+  });
+
+
+// Listen Event 4 - Double Click
+  const imgEnlarge = document.querySelector('.img-content');
+
+imgEnlarge.addEventListener('dblclick', function (event) {
+  imgEnlarge.classList.toggle('large');
+})
+
+// tried to loop this, but couldnt figure it out ^^^
+
+// Listen Event 5 - Scroll
+
+document.addEventListener('scroll',function (event) {
+    console.log('scrolled');
+  })
+
+// Listen Event 6 - Load
+document.addEventListener('load', function(event){
+    console.log('page is fully loaded');
+  })
+
